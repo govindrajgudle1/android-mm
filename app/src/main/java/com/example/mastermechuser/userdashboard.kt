f264c7cc2436b5b2543ec4b66df04b483dcd28e3
govindrajgudle1 <118347655+govindrajgudle1@users.noreply.github.com>
2023-01-19 00:37:50 +0530

    navigationhogayabanake

--- app/src/main/java/com/example/mastermechuser/userdashboard.kt
+++ app/src/main/java/com/example/mastermechuser/userdashboard.kt
@@ -2,10 +2,31 @@ package com.example.mastermechuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class userdashboard : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_userdashboard)
+        drawerLayout=findViewById(R.id.drawer1)
+        actionBarDrawerToggle= ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
