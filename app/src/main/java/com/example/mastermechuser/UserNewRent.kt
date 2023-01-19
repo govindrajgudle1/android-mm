a1960e12ad06984033b74063757d38de2f6bd691
govindrajgudle1 <118347655+govindrajgudle1@users.noreply.github.com>
2023-01-19 23:09:35 +0530

    Navigationsidewala

--- app/src/main/java/com/example/mastermechuser/UserNewRent.kt
+++ app/src/main/java/com/example/mastermechuser/UserNewRent.kt
@@ -2,10 +2,27 @@ package com.example.mastermechuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mastermechuser.databinding.ActivityForgotpasswordBinding
+import com.example.mastermechuser.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+
 
 class UserNewRent : AppCompatActivity() {
+private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_new_rent)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+    }
+    private fun replacefragment(fragment:Fragment){
+        val fragmentManager= supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.constraintrent,fragment)
+        fragmentTransaction.commit()
     }
 }
